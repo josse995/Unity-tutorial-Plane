8dfaa2d6797cee1b1e300f9ee2172667ac3869dd
josse995 <jomore03@ucm.es>
2021-01-02 15:48:23 +0100

    Initial upload

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject Target;
+    public float damping = 1;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = Target.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //var currentAngleX = transform.eulerAngles.x;
+        //var desiredAngleX = Target.transform.eulerAngles.x;
+        //var angleX = Mathf.Lerp(desiredAngleX, currentAngleX, Time.deltaTime * damping);
+        ////var rotationX = Quaternion.Euler(angleX, 0, 0);
+
+        //var currentAngleY = transform.eulerAngles.y;
+        //var desiredAngleY = Target.transform.eulerAngles.y;
+        //var angleY = Mathf.LerpAngle(currentAngleY, desiredAngleY, Time.deltaTime * damping);
+
+        //var rotation = Quaternion.Euler(angleX, 0, 0);
+        ////print(rotationY);
+
+        //transform.position = Target.transform.position - (rotation * offset);
+
+        //transform.LookAt(Target.transform);
+        
+        var currentRotation = transform.rotation;
+        currentRotation.z = 0;
+        var desiredRotation = Target.transform.rotation;
+
+        var newRotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * damping);
+        newRotation.z = 0;
+        transform.rotation = newRotation;
+        transform.position = Target.transform.position - (newRotation * offset);
+        //transform.LookAt(Target.transform);
+
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private bool rotationInputLeft;
+    private bool rotationInputRight;
+    public float speed = 1;
+    private float turnSpeed = 45;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        rotationInputLeft = Input.GetKey(KeyCode.E);
+        rotationInputRight = Input.GetKey(KeyCode.Q);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.right, turnSpeed * verticalInput * Time.deltaTime);
+
+        if (rotationInputLeft)
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
+        if (rotationInputRight)
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+    }
+}
